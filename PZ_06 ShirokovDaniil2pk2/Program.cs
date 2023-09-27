d1b7063e11aa1611873234e2a53db2d2f3b732a7
User <User@DESKTOP-AMGP8UK>
2023-09-27 15:28:59 +0500

    Добавьте файлы проекта.

---
+++ PZ_06 ShirokovDaniil2pk2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[10];
+        Random random = new Random();
+        // Заполнение массива случайными числами
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-10, 11);
+        }
+        Console.WriteLine("Исходный массив:");
+            PrintArray(array);
+        // Циклический сдвиг вправо
+        int lastElement = array[array.Length - 1];
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            array[i] = array[i - 1];
+        }
+        array[0] = lastElement;
+        Console.WriteLine("Массив после цикличекого сдвига вправо:");
+            PrintArray(array);
+    }
+            static void PrintArray(int[] array) 
+            {
+                foreach (int element in array)
+                {
+                    Console.WriteLine(element + " ");
+                }
+                Console.WriteLine();
+            }
+}
